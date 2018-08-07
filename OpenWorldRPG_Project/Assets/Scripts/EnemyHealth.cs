4c5bcadab7d32ce22b153e72beaa28665869790e
DENoofa <40660415+DENoofa@users.noreply.github.com>
2018-08-07 21:01:37 +1000

    Added in healthscripts and UI additions

---
+++ OpenWorldRPG_Project/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health Attributes")]
+    public int m_health = 1;                 // set this value in the inspector
+    public int m_currenthealth;              // Current hp
+
+
+    [Header("Death Attribute")]
+    public int m_fallspeed = 2;              // The speed in which the enemy disappears when death occurs
+    public AudioClip m_damagesound;          // Soundclip on damage
+    public AudioClip m_deathclip;            // Soundclip on death
+    public int m_exp = 1;                    // set this value in the inspector
+    public Slider m_enemyslider;             // enemy health slider (to be fixed)
+
+
+    [Header("Animator reference")]
+    Animator m_anim;                         // Animator
+    AudioSource m_enemysounds;               // Sound of enemy when hit
+    AudioSource m_soundenemy;                // Sound of enemy when death
+    CapsuleCollider m_capsule;               // Ref to capsule collider
+    ParticleSystem m_particle;               // Ref to particle system
+
+
+
+    bool m_falling;                          // Play a animation for the enemy to disappear
+    bool m_dead;                             // You know, just to make sure they are dead
+
+
+    void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_enemysounds = GetComponent<AudioSource>();
+        m_soundenemy = GetComponent<AudioSource>();
+        m_particle = GetComponentInChildren<ParticleSystem>();
+        m_capsule = GetComponent<CapsuleCollider>();
+
+        m_currenthealth = m_health;
+    }
+
+
+    void Update()
+    {
+        if (m_falling)
+        {
+            transform.Translate(-Vector3.up * m_fallspeed * Time.deltaTime);
+        }
+    }
+
+
+    public void Takedamage(int amount, Vector3 hitPoint)
+    {
+        if (m_dead)
+            return;
+
+        m_health -= amount;
+        m_particle.transform.position = hitPoint;
+        m_particle.Play();
+        m_enemysounds.clip = m_damagesound;
+
+        if (m_currenthealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        m_dead = true;
+        m_capsule.isTrigger = true;
+        m_anim.SetTrigger("Dead");
+        m_soundenemy.clip = m_deathclip;
+        m_soundenemy.Play();
+        m_exp 
+    }
+
+    public void m_actualfalling()
+    {
+        GetComponent<NavMeshAgent>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        m_falling = true;
+        Destroy(gameObject, 5f);
+    }
+
+}
---
+++ OpenWorldRPG_Project/Assets/Scripts/Health.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    [Header("Health")]
+    public int our_health = 1;                                      // set this in the inspector
+    public int our_currenthealth;                                   // our current health
+    public Slider ourhealthslider;                                  // add the component to the inspector
+
+    [Header("Mana")]
+    public int our_mana = 1;                                        // set this in the inspector
+    public int our_currentmana;                                     // our current mana
+    public Slider ourmanaslider;                                    // add the component to the inspector
+
+
+    [Header("Experience Components")]
+    public int our_currentexp = 1;                                  // set this in the inspector
+    public int our_exp;
+    public Slider ourexpslider;                                     // add the component to the inspector
+
+    [Header("Damage Indicator")]
+    public float our_damagespeed = 5f;                              // this will make the screen flash when we take damage
+    public Image our_damageimage;                                   // this will show an image when we take damage
+    public Color our_damagecolour = new Color(1f, 0f, 0f, 0.1f);    // red, with an alpha of .1
+
+    bool our_damage;
+    bool our_manause;
+    bool our_death;
+
+    [Header("Animator Reference")]
+    Animator m_anim;                                                // Animator component
+    AudioSource m_audio;                                            // Audio component
+    public AudioClip ourdeathclip;                                  // Our death sound
+    // Playermovement m_playermovement;                             // Reference to the movement
+
+
+
+
+
+    private void Awake()
+    {
+        m_anim = GetComponent<Animator>();
+        m_audio = GetComponent<AudioSource>();
+        //m_playermovement = GetComponent<Playermovement>;
+
+        our_currenthealth = our_health;
+        our_currentmana = our_mana;
+        our_currentexp = our_exp;
+    }
+
+
+    private void Update()
+    {
+        if (our_damage)
+        {
+            our_damageimage.color = our_damagecolour;
+        }
+        else
+        {
+            our_damageimage.color = Color.Lerp(our_damageimage.color, Color.clear, our_damagespeed * Time.deltaTime);
+        }
+        our_damage = false;
+    }
+
+    public void TakeDamage (int m_amount)
+    {
+        our_damage = true;
+        our_currenthealth -= m_amount;
+        ourhealthslider.value = our_currenthealth;
+        m_audio.Play();
+        if(our_currenthealth <= 0 && !our_death)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        our_death = true;
+        m_anim.SetTrigger("Dead");
+        m_audio.clip = ourdeathclip;
+        m_audio.Play();
+
+        //m_playermovement.enabled = false
+    }
+}
