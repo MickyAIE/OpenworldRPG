a4894aeadbe943337ee04f2bd8d10839a06dc55c
DENoofa <40660415+DENoofa@users.noreply.github.com>
2018-08-09 19:51:03 +1000

    Enemyhealth bar added

--- OpenWorldRPG_Project/Assets/Scripts/EnemyHealth.cs
+++ OpenWorldRPG_Project/Assets/Scripts/EnemyHealth.cs
@@ -10,13 +10,14 @@ public class EnemyHealth : MonoBehaviour
     [Header("Health Attributes")]
     public int m_health = 1;                 // set this value in the inspector
     public int m_currenthealth;              // Current hp
+    public float m_healthbar;
 
 
     [Header("Death Attribute")]
     public int m_fallspeed = 2;              // The speed in which the enemy disappears when death occurs
     public AudioClip m_damagesound;          // Soundclip on damage
     public AudioClip m_deathclip;            // Soundclip on death
-    public int m_enemyexpgiven = 1;                    // experience of the enemy
+    public int m_enemyexpgiven = 1;          // experience of the enemy
     public Slider m_enemyslider;             // enemy health slider (to be fixed)
 
 
@@ -32,6 +33,11 @@ public class EnemyHealth : MonoBehaviour
     bool m_falling;                          // Play a animation for the enemy to disappear
     bool m_dead;                             // You know, just to make sure they are dead
 
+
+    private void Start()
+    {
+        m_healthbar = Screen.width / 8;
+    }
 
     void Awake()
     {
@@ -52,6 +58,7 @@ public class EnemyHealth : MonoBehaviour
         {
             transform.Translate(-Vector3.up * m_fallspeed * Time.deltaTime);
         }
+        m_healthbarcurrent(0);
     }
 
 
@@ -69,6 +76,31 @@ public class EnemyHealth : MonoBehaviour
         {
             Death();
         }
+    }
+
+    private void OnGUI()
+    {
+        Vector2 m_targetpostition;
+        m_targetpostition = Camera.main.WorldToScreenPoint (transform.position);
+
+        GUI.Box(new Rect(700, 10, m_healthbar, 20), m_currenthealth + "/" + m_health);
+    }
+
+
+    public void m_healthbarcurrent(int make)
+    {
+        m_currenthealth += make;
+
+        if (m_currenthealth < 0)
+            m_currenthealth = 0;
+
+        if (m_currenthealth > m_health)
+            m_currenthealth = m_health;
+
+        if (m_health < 1)
+            m_health = 1;
+
+        m_healthbar = (Screen.width / 8) * (m_currenthealth / (float)m_health);
     }
 
     void Death()
