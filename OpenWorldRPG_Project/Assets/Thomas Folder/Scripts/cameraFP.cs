042a857a2734904c0d4bf20ed8ef855b184b5164
TrueGamingGalaxy <37901032+TrueGamingGalaxy@users.noreply.github.com>
2018-08-09 22:07:41 +0930

    Player Movement
    -Test Camera
    -Idle, walking and running w/ anims

---
+++ OpenWorldRPG_Project/Assets/Thomas Folder/Scripts/cameraFP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFP : MonoBehaviour {
+
+    Vector2 mouseLook;
+    Vector2 smoothV;
+    public float sensetivity = 5f;
+    public float smoothing = 2f;
+
+    GameObject character;
+
+	void Start ()
+    {
+        character = this.transform.parent.gameObject;
+	}
+	
+
+	void Update ()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensetivity * smoothing, sensetivity * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, -70f, 100f);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ OpenWorldRPG_Project/Assets/Thomas Folder/Scripts/playerController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    public float StartSpeed = 10f;
+    public float speed;
+    public float Rspeed = 13f;
+    public bool running = false;
+    private Animator playeranim;
+
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        playeranim = GetComponent<Animator>();
+        running = false;
+        speed = StartSpeed;
+	}
+	
+	void Update () {
+
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+
+        if(Input.GetButton("Fire3"))
+        {
+            if (straffe != 0)
+            {
+                playeranim.SetBool("isWalking", false);
+                playeranim.SetBool("isIdle", false);
+                playeranim.SetBool("isRunning", true);
+            }
+            else
+            {
+                playeranim.SetBool("isIdle", true);
+                playeranim.SetBool("isWalking", false);
+                playeranim.SetBool("isRunning", false);
+            }
+
+            if (translation != 0)
+            {
+                playeranim.SetBool("isWalking", false);
+                playeranim.SetBool("isIdle", false);
+                playeranim.SetBool("isRunning", true);
+            }
+            running = true;
+            speed = Rspeed;
+
+        }
+        else
+        {
+            running = false;
+            speed = StartSpeed;
+        }
+
+        if (running == false)
+        {
+            if (straffe != 0)
+            {
+                playeranim.SetBool("isWalking", true);
+                playeranim.SetBool("isIdle", false);
+                playeranim.SetBool("isRunning", false);
+            }
+            else
+            {
+                playeranim.SetBool("isIdle", true);
+                playeranim.SetBool("isWalking", false);
+                playeranim.SetBool("isRunning", false);
+            }
+
+            if (translation != 0)
+            {
+                playeranim.SetBool("isWalking", true);
+                playeranim.SetBool("isIdle", false);
+                playeranim.SetBool("isRunning", false);
+            }
+        }
+        else
+        {
+            playeranim.SetBool("isIdle", false);
+            playeranim.SetBool("isWalking", false);
+        }
+
+
+
+
+
+
+
+    }
+}
