8b40e59ec8ae47207769247e5f5d77adee656747
Neil <37834094+AgentneilPLAGUE@users.noreply.github.com>
2018-08-10 01:59:46 -0700

    script modification? maybe
    Pip, wanna take a look

--- OpenWorldRPG_Project/Assets/Scripts/Experience.cs
+++ OpenWorldRPG_Project/Assets/Scripts/Experience.cs
@@ -50,3 +50,65 @@ public class Experience : MonoBehaviour
         m_text.text = "Level : " + m_level.ToString();
     }
 }
+
+/*   // Wanna check this out Pip? I probably overcomplicated it, but hey :D maths (Loved your code too) This took me ages btw, even with your code lol
+ {
+    public Text levelText;
+    public Text experienceText;
+    public Text requiredExperienceText;
+    public Slider experienceSlider;
+
+    public static int experience;
+    int requiredExperience;             //To level up
+    int playerLevel;
+    public float experienceGrowth;
+
+    private void Start()
+    { 
+        playerLevel = 1;
+        levelText.text = "Lv 1";
+
+        experience = 0;
+        experienceText.text = "" + experience;
+
+        requiredExperience = 100;
+        requiredExperienceText.text = "/" + requiredExperience;
+
+        experienceSlider.value = experience;
+        experienceSlider.maxValue = requiredExperience;
+
+        experienceGrowth = 1.50f;
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            experience += 20;
+            experienceSlider.value = experience;
+            experienceText.text = "" + experience;
+        }
+
+      
+        if (experienceSlider.value >= experienceSlider.maxValue)
+        {
+            LevelUp();
+        }
+    }
+
+    void LevelUp()
+    {
+        experience = experience -= requiredExperience;
+        experienceSlider.value = experience;
+
+        requiredExperience = (int)(requiredExperience * experienceGrowth);
+        experienceSlider.maxValue = requiredExperience;
+
+        playerLevel += 1;
+        levelText.text = "Lv " + playerLevel;
+
+        requiredExperienceText.text = "/" + requiredExperience;
+    }
+}
+*/
