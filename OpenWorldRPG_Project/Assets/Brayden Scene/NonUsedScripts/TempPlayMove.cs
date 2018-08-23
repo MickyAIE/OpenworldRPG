eb76600d49cc504b4679ba874fcb274d28a15c4c
Brayden Griffiths <brayden.jay.griffiths@gmail.com>
2018-08-23 16:24:04 +0930

    Basic Enemy Follow Template

---
+++ OpenWorldRPG_Project/Assets/Brayden Scene/NonUsedScripts/TempPlayMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayMove : MonoBehaviour {
+
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+      //  Player.transform.position();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
