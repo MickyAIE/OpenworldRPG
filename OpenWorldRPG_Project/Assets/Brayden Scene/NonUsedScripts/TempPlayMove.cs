833679c259b21e6c6915336432f010c161c6c872
Brayden Griffiths <brayden.jay.griffiths@gmail.com>
2018-08-23 17:30:11 +0930

    More Basic Temp

--- OpenWorldRPG_Project/Assets/Brayden Scene/NonUsedScripts/TempPlayMove.cs
+++ OpenWorldRPG_Project/Assets/Brayden Scene/NonUsedScripts/TempPlayMove.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class TempPlayMove : MonoBehaviour {
 
-    GameObject Player;
+    public GameObject Player;
 
-	// Use this for initialization
-	void Start () {
-      //  Player.transform.position();
-	}
-	
+    Rigidbody rigid;
+    public float m_Speed = 5.0f;
+    public float m_Input;
+    //rigidbody = GetComponent<Rigidbody>();
+    // Use this for initialization
+    
+    void Awake ()
+    {
+        rigid = GetComponent<Rigidbody>();
+
+        //rigidbody.velocity = transform.forward;
+        // int x = Player.transform.position();
+        //  Player.transform.position();
+    }
+	private void OnEnable()
+    {
+        rigid.isKinematic = false;
+
+        m_Input = 0f;
+        
+    }
+    private void OnDisable()
+    {
+        rigid.isKinematic = true;
+    }
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        m_Input = Input.GetAxis("Vertical");
+        //if(KeyCode)
+		//Player.transform.position += Vector3.forward * 5 * Time.deltaTime;
 	}
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        Vector3 movement = Player.transform.forward * m_Input * m_Speed * Time.deltaTime;
+        rigid.MovePosition(rigid.position + movement);
+    }
 }
