a1279afedd61492a0e4f528cdc384ff770b12a79
TrueGamingGalaxy <37901032+TrueGamingGalaxy@users.noreply.github.com>
2018-08-28 21:40:50 +0930

    TextMeshPro + Basic Quest Giver Template + Basic Dialogue Template

---
+++ OpenWorldRPG_Project/Assets/Thomas Folder/Scripts/Dialogue.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour {
+
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+    public GameObject nextb;
+
+    private void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    private void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            nextb.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        nextb.SetActive(false);
+        
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        } else
+        {
+            textDisplay.text = "";
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+}
---
+++ OpenWorldRPG_Project/Assets/Thomas Folder/Scripts/testNpcDialogue.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testNpcDialogue : MonoBehaviour {
+
+    public Animator npcanim;
+    public bool interactable;
+    public GameObject QuestMarker;
+    public GameObject Dialogue;
+    public GameObject Interact;
+
+
+
+	void Start () {
+        npcanim = GetComponent<Animator>();
+        npcanim.SetBool("IsIdle", false);
+        interactable = false;
+	}
+
+    private void Update()
+    {
+        if (interactable == true && Input.GetKeyDown(KeyCode.F))
+        {
+            print("F");
+            QuestMarker.SetActive(false);
+            Dialogue.SetActive(true);
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            npcanim.SetBool("IsIdle", true);
+            npcanim.SetBool("IsWaving", false);
+        }
+        else
+            return;
+        interactable = true;
+        Interact.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            npcanim.SetBool("IsIdle", false);
+            npcanim.SetBool("IsWaving", true);
+        }
+        else
+            return;
+        interactable = false;
+        Interact.SetActive(false);
+    }
+
+}
