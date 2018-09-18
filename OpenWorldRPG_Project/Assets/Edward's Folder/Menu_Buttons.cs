1909ad378874488d1fc265dcd441e4b8c2aca878
EdwardAIE <40660352+EdwardAIE@users.noreply.github.com>
2018-09-18 16:48:52 +1000

    update

---
+++ OpenWorldRPG_Project/Assets/Edward's Folder/Menu_Buttons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Menu_Buttons : MonoBehaviour {
+
+    public Button Quit;
+    public Button Play;
+    public Button Settings;
+
+    // Use this for initialization
+    void Start() {
+        Button qbtn = Quit.GetComponent<Button>();
+        qbtn.onClick.AddListener(QuitProgram);
+
+        Button pbtn = Play.GetComponent<Button>();
+        pbtn.onClick.AddListener(StartGame);
+
+        Button sbtn = Settings.GetComponent<Button>();
+        sbtn.onClick.AddListener(QuitProgram);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void QuitProgram()
+    {
+        Application.Quit();
+    }
+
+    void StartGame()
+    {
+
+        SceneManager.LoadScene("Demo_Scene");
+    }
+
+    void OpenSettings()
+    {
+
+    }
+}
